44eba757c5a4bc86f442c2a13f2768fbd524d933
Adam McFry <armcfry@gmail.com>
2020-09-21 21:33:13 -0500

    start menu, help screen

--- The Domino Effect/ViewController.swift
+++ The Domino Effect/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, HelpViewControllerProtocol {
 
+    
+
+    @IBOutlet weak var soundButton: UIButton!
+    @IBOutlet weak var settingButton: UIButton!
+    @IBOutlet weak var helpButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBAction func helpTap(_ sender: UIButton) {
+        let button = sender as UIButton
+        if button.tag == 1
+        {
+            helpDialogue!.titleText = "ABOUT"
+            helpDialogue!.feedbackText = "The Domino Effect is an iOS application that sorts a collection of user-specified dominoes. The use case for this application will be a companion when playing a round of dominoes. Press start, then select the dominoes you wish to sort, then press sort."
+            helpDialogue!.buttonText = "DISMISS";
+            DispatchQueue.main.async
+            {
+                self.present(self.helpDialogue!, animated: true, completion: nil)
+            }
+        }
+    }
+    var helpDialogue:HelpViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Initialize the result dialog
+        helpDialogue = storyboard?.instantiateViewController(identifier: "HelpVC") as? HelpViewController
+        helpDialogue?.modalPresentationStyle = .overCurrentContext
+        helpDialogue?.delegate = self
         // Do any additional setup after loading the view.
     }
 
 
+func dialogueDismissed() {
+    print("dismiss");
 }
-
+}
